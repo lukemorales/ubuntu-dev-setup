f8e9b53c5ec8b21b4348b2a7910111440b4454a9
Luke Morales <lukemorales@live.com>
2020-06-19 20:54:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Linux Workstation Setup
+
+This configs were made based on [Erick Wendel's setup](https://github.com/ErickWendel/ew-ubuntu-setup) and my own needs as a JS Developer and other personal stuff.
+
+## Getting Started
+
+Just clone this repo or download and execute 
+
+```
+sh startup.sh
+```
+
+## You'll be stalling...
+
+* [Curl](https://curl.haxx.se/) - Command line tool and library for transferring data with URLs
+* [Git](https://git-scm.com/) - Free and open source distributed version control system
+* [Vim](https://www.vim.org/) - Text editor
+* [Oh-My-Zsh](https://ohmyz.sh/) - Framework for managing your ZSH configuration
+* [Xclip](https://opensource.com/article/19/7/xclip) - Command line interface to the X11 clipboard
+* [VsCode](https://code.visualstudio.com/) - Source-code editor made by Microsoft
+* [Spotify](https://www.spotify.com/) - Music streaming and media services provider
+* [NVM](https://github.com/nvm-sh/nvm) - Version manager for NodeJS
+* [NodeJS](https://nodejs.org/en/) - JavaScript runtime built on Chrome's V8 JavaScript engine
+* [Autosuggestions](https://github.com/zsh-users/zsh-autosuggestions) - It suggests commands as you type based on history and completions
+* [Franz](https://meetfranz.com/) - Group messaging services into one application
+* [Terminator](https://terminator-gtk3.readthedocs.io/en/latest/) - Terminal emulator like xterm, gnome-terminal, konsole, etc
+* [Docker](https://www.docker.com/) - Platform for building, deploying, and managing containerized applications
+* [Docker Compose](https://docs.docker.com/compose/) - Tool for defining and running multi-container Docker applications
+* [Fzf](https://github.com/junegunn/fzf) - General-purpose command-line fuzzy finder
+* [DBeaver](https://dbeaver.io/) - Universal DB tool
+* [Postman](https://www.postman.com/) - API Client
+* [VLC](https://www.videolan.org/vlc/) - Media player
+* [Transmission](https://transmissionbt.com/) - Torrent client
+* [Kubernetes CLI](https://kubernetes.io/docs/reference/kubectl/overview/) - CLI to control Kubernetes clusters
+* [AWS CLI](https://aws.amazon.com/pt/cli/) - CLI to control AWS services
+* [Heroku CLI](https://devcenter.heroku.com/categories/command-line) - Create and manage your Heroku apps
+* [Fira Code](https://github.com/tonsky/FiraCode) - Monospaced font with programming ligatures
+* [Typescript](https://www.typescriptlang.org/) - Typed superset of JavaScript that compiles to plain JavaScript.
+* [Create React App](https://github.com/facebook/create-react-app) - Set up a modern ReactJS web app by running one command
+* [Robo3t](https://robomongo.org/) - MongoDB GUI
+
+## Authors
+
+* **Erick Wendel** - *Initial work* - [Erick Wendel](https://github.com/ErickWendel)
+
+* **Olavio Lacerda** - Additions and modifications
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ startup.sh
@@ -0,0 +1,218 @@
+echo "Welcome! Let's start setting up your system xD It could take more than 10 minutes, be patient"
+
+sudo apt-get update
+
+echo 'installing curl' 
+sudo apt install curl -y
+
+echo 'installing vim'
+sudo apt install vim -y
+clear
+
+echo 'installing git' 
+sudo apt install git -y
+
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"Olavio Lacerda\""
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"olavio.lacerda@hotmail.com\""
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
+
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, no problem. :) Let's move on!"
+fi
+
+echo "Generating a SSH Key"
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo 'enabling workspaces for both screens' 
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+echo 'installing zsh'
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+
+echo 'installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
+
+export alias pbcopy='xclip -selection clipboard'
+export alias pbpaste='xclip -selection clipboard -o'
+source ~/.zshrc
+
+echo 'installing code'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y # or code-insiders
+
+echo 'installing extensions'
+code --install-extension Shan.code-settings-sync
+
+echo 'installing spotify' 
+snap install spotify
+
+echo 'installing chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo 'installing nvm' 
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+nvm --version
+nvm install 12
+nvm alias default 12
+node --version
+npm --version
+
+echo 'installing Typescript'
+npm install -g typescript
+
+echo 'installing Create React App'
+npm install -g create-react-app
+
+echo 'installing autosuggestions' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing theme'
+sudo apt install fonts-firacode -y
+wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
+
+echo 'installing franz' 
+wget https://github.com/meetfranz/franz/releases/download/v5.1.0/franz_5.1.0_amd64.deb -O franz.deb
+sudo dpkg -i franz.debchristian-kohler.path-intellisense
+sudo apt-get install -y -f 
+
+echo 'installing terminator'
+sudo apt-get update
+sudo apt-get install terminator -y
+
+echo 'adding dracula theme' 
+cat <<EOF >  ~/.config/terminator/config
+[global_config]
+  title_transmit_bg_color = "#ad7fa8"
+[keybindings]
+  close_term = <Primary>w
+  close_window = <Primary>q
+  new_tab = <Primary>t
+  new_window = <Primary>i
+  paste = <Primary>v
+  split_horiz = <Primary>e
+  split_vert = <Primary>d
+  switch_to_tab_1 = <Primary>1
+  switch_to_tab_10 = <Primary>0
+  switch_to_tab_2 = <Primary>2
+  switch_to_tab_3 = <Primary>3
+  switch_to_tab_4 = <Primary>4
+  switch_to_tab_5 = <Primary>5
+  switch_to_tab_6 = <Primary>6
+[layouts]
+  [[default]]
+    [[[child1]]]
+      parent = window0
+      type = Terminal
+    [[[window0]]]
+      parent = ""
+      type = Window
+[plugins]
+[profiles]
+  [[default]]
+    cursor_color = "#aaaaaa"
+EOF
+
+
+cat <<EOF >>  ~/.config/terminator/config
+[[Dracula]]
+    background_color = "#1e1f29"
+    background_darkness = 0.88
+    background_type = transparent
+    copy_on_selection = True
+    cursor_color = "#bbbbbb"
+    foreground_color = "#f8f8f2"
+    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
+    scrollback_infinite = True
+EOF
+
+echo 'installing docker' 
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+chmod 777 /var/run/docker.sock
+docker run hello-world
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'installing kubectl'
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+echo 'installing heroku-cli'
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+heroku --version
+
+echo 'installing aws-cli' 
+sudo apt-get install awscli -y
+aws --version
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+sudo dpkg -i session-manager-plugin.deb
+session-manager-plugin --version
+
+echo 'installing fzf'
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+echo 'installing dbeaver'
+wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
+sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
+sudo apt-get install -f
+
+echo 'installing Robo3t'
+snap install robo3t-snap
+
+echo 'installing Postman' 
+snap install postman
+
+echo 'installing vlc'
+sudo apt install vlc -y
+sudo apt install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
+
+echo 'installing transmission'
+sudo add-apt-repository ppa:transmissionbt/ppa
+sudo apt-get update
+sudo apt-get install transmission transmission-qt -y
+
+clear 
+
+echo 'All setup, enjoy!'
